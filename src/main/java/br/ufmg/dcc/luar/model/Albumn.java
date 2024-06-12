04a056f2a25fd557130ed0c41cb4e8af0414c5c2
flaviosoriano <flaviosoriano0@gmail.com>
2024-06-12 01:10:35 -0300

    Entity Mapping JPA

--- src/main/java/br/ufmg/dcc/luar/model/Albumn.java
+++ src/main/java/br/ufmg/dcc/luar/model/Albumn.java
@@ -2,12 +2,31 @@ package br.ufmg.dcc.luar.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="albumn")
 public class Albumn implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	
+	
+	private int artistId;
+	
+	@Column
 	private String title;
+	
+	@Column(name="Cover")
 	private String coverImageFileUrl;
 	
 	public int getId() {
@@ -27,6 +46,12 @@ public class Albumn implements Serializable {
 	}
 	public void setCoverImageFileUrl(String coverImageFileUrl) {
 		this.coverImageFileUrl = coverImageFileUrl;
+	}
+	public int getArtistId() {
+		return artistId;
+	}
+	public void setArtistId(int artistId) {
+		this.artistId = artistId;
 	}
 	
 }
--- src/main/java/br/ufmg/dcc/luar/model/Artist.java
+++ src/main/java/br/ufmg/dcc/luar/model/Artist.java
@@ -2,12 +2,24 @@ package br.ufmg.dcc.luar.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="artist")
 public class Artist implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
+	@Column
 	private String name;
 
 	public int getId() {
