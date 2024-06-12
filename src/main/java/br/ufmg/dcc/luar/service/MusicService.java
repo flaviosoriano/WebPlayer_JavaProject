5a419c8e4a46b2665d82370431da7f1d6bf83cd0
flaviosoriano <flaviosoriano0@gmail.com>
2024-06-12 01:11:20 -0300

    fix: Adjusting index page

---
+++ src/main/java/br/ufmg/dcc/luar/service/MusicService.java
@@ -0,0 +1,31 @@
+package br.ufmg.dcc.luar.service;
+
+import java.util.List;
+
+import br.ufmg.dcc.luar.model.Track;
+
+public class MusicService {
+	
+	public List<String> listDiscografy(int artistId){
+		List<String> names = null;
+		return names;
+	}
+	
+	public List<String> listAllTracks(){
+		List<String> names = null;
+		return names;
+	}
+	
+	public int addTrack(Track newTrack) {
+		int trackId = 0;
+		return trackId;
+	}
+	
+	public void removeTrack(int trackId) {
+		
+	}
+	
+	public void updateTrack(int trackId) {
+		
+	}
+}
